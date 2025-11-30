ba165ea3196756513311316d4ceac6a763e6d6cb
Chrizhan <channnquidato@gmail.com>
2025-12-01 07:46:56 +0800

    Import api and createPageUrl in About.jsx

--- frontend/src/Pages/About.jsx
+++ frontend/src/Pages/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
+import api from '@/api/backend';
 import { createPageUrl } from '@/utils';
 import { 
     Brain, 
